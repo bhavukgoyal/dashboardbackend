9857fab6c173000ff8cbcfa808f9020a653d791e
Bhavuk Goyal <goyalbhavuk19@gmail.com>
2024-03-20 18:11:25 +0530

    i am the best

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 }));
 
 app.post('/sector', async (req, res) => {
