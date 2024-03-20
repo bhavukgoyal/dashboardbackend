c8bc20b4b1d1f55e12d853cf9a8a380b44b903c1
Bhavuk Goyal <goyalbhavuk19@gmail.com>
2024-03-20 12:16:44 +0530

    mybackend

--- App.js
+++ App.js
@@ -6,13 +6,30 @@ const app = express();
 
 
 app.post('/sector', async (req, res) => {
-  const field = req.body.field; 
+  const sector = req.body.sector; 
 
   try {
-    const filteredValues = await Data.find({"sector":field});
-    res.status(200).json(filteredValues);
+    const distinctTopics = await Data.distinct("topic", { "sector": sector });
+    res.status(200).json(distinctTopics);
   } catch (error) {
     res.status(500).json({ message: 'An error occurred while fetching distinct values' });
+  }
+});
+
+app.post('/topic', async (req, res) => {
+  const sector = req.body.sector;
+  const topic = req.body.topic;
+
+  try {
+    const endDate = await Data.findOne({ "sector": sector, "topic": topic }, { end_year: 1, _id: 0 }).sort({ end_year: -1 });
+    
+    if (endDate) {
+      res.status(200).json({ endDate: endDate.end_year });
+    } else {
+      res.status(404).json({ message: 'End date not found for the specified sector and topic' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while fetching the end date' });
   }
 });
 
