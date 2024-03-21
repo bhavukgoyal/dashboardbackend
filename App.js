7a8ac718f9418e5d43876ce639eac3c1ee034dec
Bhavuk Goyal <goyalbhavuk19@gmail.com>
2024-03-21 14:27:56 +0530

    i am a loserrr

--- App.js
+++ App.js
@@ -4,19 +4,18 @@ const Data = require('./Model.js');
 const cors=require('cors')
 const app = express();
 
-app.use(cors({
-  origin: '*',
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Origin", "Content-Type", "Accept", "Authorization", "X-Request-With"],
-}));
+// CORS middleware to allow requests from all origins
+app.use(cors());
 
 app.post('/sector', async (req, res) => {
   const sector = req.body.sector; 
 
   try {
     const distinctTopics = await Data.distinct("topic", { "sector": sector });
-    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.status(200).json(distinctTopics);
   } catch (error) {
     res.status(500).json({ message: 'An error occurred while fetching distinct values' });
@@ -34,6 +33,7 @@ app.post('/topic', async (req, res) => {
     const endDate = await Data.findOne({ "sector": sector, "topic": topic }, { end_year: 1, _id: 0 }).sort({ end_year: -1 });
     
     if (endDate) {
+      
       res.status(200).json({ endDate: endDate.end_year });
     } else {
       res.status(404).json({ message: 'End date not found for the specified sector and topic' });
