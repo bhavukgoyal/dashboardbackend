266e755407cc99cb2df3aceea67fef6e2688202a
Bhavuk Goyal <goyalbhavuk19@gmail.com>
2024-03-21 13:43:06 +0530

    i am a loser

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Data = require('./Model.js');
-const cors = require('cors');
+
 const app = express();
 
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post('/sector', async (req, res) => {
   const sector = req.body.sector; 
