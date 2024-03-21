3efacab4a686b3361de67510da6a1775a71540f6
Bhavuk Goyal <goyalbhavuk19@gmail.com>
2024-03-21 14:05:00 +0530

    happy

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Data = require('./Model.js');
-
+const cors=require('cors')
 const app = express();
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: [
+      "Origin",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+      "X-Request-With",
+    ],
+  })
+);
 
 app.post('/sector', async (req, res) => {
   const sector = req.body.sector; 
